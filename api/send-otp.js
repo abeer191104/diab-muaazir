1e7092f0fa0affc254d81da66281772d5543aa63
abeer191104 <abeeralfayez4@gmail.com>
2026-05-13 23:27:01 +0300

    Update send-otp.js

--- api/send-otp.js
+++ api/send-otp.js
@@ -47,7 +47,8 @@ export default async function handler(req, res) {
         );
 
         return res.status(200).json({
-            success: true
+            success: true,
+            otp
         });
 
     } catch(err) {
